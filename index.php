60fd7315a68d6ae851a6f216fa532916c1999035
dyomiotidashi <90453860+Dankossh@users.noreply.github.com>
2022-12-05 01:06:36 +0600

    new site

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+if ($_SESSION['user']) {
+    header('Location: profile.php');
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и регистрация</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<!-- Форма авторизации -->
+
+    <form action="vendor/signin.php" method="post">
+        <label>Логин</label>
+        <input type="text" name="login" placeholder="Введите свой логин">
+        <label>Пароль</label>
+        <input type="password" name="password" placeholder="Введите пароль">
+        <form method="post" action="write.php">
+        
+        <br><img src="captcha.php">
+        <input type="submit" value="Submit">
+        <input class="input" type="text" name="norobot" placeholder="Ответить">
+
+
+        <p>
+            У вас нет аккаунта? - <a href="/register.php">зарегистрируйтесь</a>!
+        </p>
+        <?php
+            if ($_SESSION['message']) {
+                echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+            }
+            unset($_SESSION['message']);
+        ?>
+        </form>
+    </form>
+
+</body>
+</html>
